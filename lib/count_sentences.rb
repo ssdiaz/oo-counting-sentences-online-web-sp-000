f5f26cb8978d57d77df962126c1c22be61c8f3bc
Sam D <samidiaz3@gmail.com>
2021-03-01 01:47:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences   # get back a count of sentences on a string
-    self.split(".", "?", "!").count.strip
+    self.split(".", "?", "!")#.count.strip
 
     binding.pry
   end
