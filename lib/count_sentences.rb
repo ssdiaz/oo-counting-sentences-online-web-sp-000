e500a54083163407885175ee1fcc63c8701afb1c
Sam D <samidiaz3@gmail.com>
2021-03-01 01:49:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,7 @@ class String
   def count_sentences   # get back a count of sentences on a string
     self.split(".", "?", "!")#.count.strip
 
+#Ruby lib/count_sentences.rb
     puts "hi"
     binding.pry
   end
