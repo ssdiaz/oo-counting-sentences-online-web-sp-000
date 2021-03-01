93d64af0d9eff6bf44f4146bd6aa80ab7a08c876
Sam D <samidiaz3@gmail.com>
2021-03-01 02:39:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,43 @@ require 'pry'
 
 class String
 
-  def sentence?
+  def sentence?     # true if ends in . false if not
+    self.end_with?(".")
+  end
+
+  def question?   # true if ends with ? false if not
+    self.end_with?("?")
+  end
+
+  def exclamation?  # true if ends with ! false if not
+    self.end_with?("!")
+  end
+
+  def count_sentences   # get back a count of sentences on a string
+    # To use regex:
+    #   searching for ( . ! ? )
+    #   start of regex search uses / and at end /
+    #     (/ . ! ? /)
+    #   beginning of each criteria is \
+    #     (/ \. \! \? /)
+    #   To identify/seperate criteria, use |
+    #     (/ \. | \! | \? /)
+    #   Clean up spaces otherwise won't work
+    #     (/\.|\!|\?/)
+
+    new_array =  self.split(/\.|\!|\?/)
+    counter_array = []
     
-  end
+    new_array.each do |sentence|
+      if sentence.length > 1
+        counter_array << sentence
+      end
+    end
 
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
+    counter_array.count
 
   end
 end
+
+# solution online:
+  # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
